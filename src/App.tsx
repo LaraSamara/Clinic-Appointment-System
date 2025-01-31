7b34c73a1e956f43cfd0af58f7f1ee68859cd628
Osama Maree <osamamarei2001@gmail.com>
2025-02-01 00:01:24 +0200

    Add Base Project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import TestComponent from "./test-component";
+
+function App() {
+  return (
+    <>
+      <TestComponent />
+    </>
+  );
+}
+
+export default App;
---
+++ src/test-component/test-component.tsx
@@ -0,0 +1,7 @@
+import classes from "./style.module.css";
+
+const TestComponent = () => {
+  return <div className={classes.TestComponent}>test-component</div>;
+};
+
+export default TestComponent;
