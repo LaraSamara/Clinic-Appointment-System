447434bfb3766654f11a214d42c7fab260a2afb7
Lara Samara <136354923+LaraSamara@users.noreply.github.com>
2025-02-12 19:05:41 +0200

    CA-23: Add Boundary Component (#22)
    * CA-23: Add Boundary Component
    
    * CA-23: Add Boundary Component

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,24 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SnackbarProvider from "./context/SnackbarContext";
 import { GridProvider } from "./context/GridContext";
+import ErrorBoundary from "./component/errorBoundary";
+import App from "./App";
 import './index.css';
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <SnackbarProvider>
-        <GridProvider>
-        <Routes>
-          <Route path="/*" element={<App />} />
-        </Routes>
-        </GridProvider>
-      </SnackbarProvider>
+      <ErrorBoundary>
+        <SnackbarProvider>
+          <GridProvider>
+          <Routes>
+            <Route path="/*" element={<App />} />
+          </Routes>
+          </GridProvider>
+        </SnackbarProvider>
+      </ErrorBoundary>
     </BrowserRouter>
   </StrictMode>
 );
